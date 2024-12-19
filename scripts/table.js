e0655b4ba3bbb26a56f1256f2af9be632c17aa5e
Robert Ciocan <ciocan.robertt@gmail.com>
2024-12-19 09:52:57 +0200

    table WIP

--- scripts/table.js
+++ scripts/table.js
@@ -19,12 +19,18 @@ document.querySelector('.table').innerHTML=`
           <th scope="col">Stergere</th>
       </tr>
   </th>
-  <tbody>
-       <tr>
-      <td>1</td> <td>Sistem</td><td>Cod</td><td>eticheta</td><td>Dimensiune</td><td>Buc</td><td>Cantitate</td><td>$Suprafata</td><td>Suprafata Totala</td><td>Flansa</td><td>Observatii</td><td><button class="quantity-increase css-quantity-increase js-quantity-increase">+</button> <button class="js-quantity-decrease quantity-decrease css-quantity-decrease">-</button></td><td><button class="quantity-delete css-quantity-delete js-quantity-delete" data-productid=>Sterge produs</button></td>
-      </tr>
-       <tr>
-      <td>1</td> <td>Sistem</td><td>Cod</td><td>eticheta</td><td>Dimensiune</td><td>Buc</td><td>Cantitate</td><td>$Suprafata</td><td>Suprafata Totala</td><td>Flansa</td><td>Observatii</td><td><button class="quantity-increase css-quantity-increase js-quantity-increase">+</button> <button class="js-quantity-decrease quantity-decrease css-quantity-decrease">-</button></td><td><button class="quantity-delete css-quantity-delete js-quantity-delete" data-productid=>Sterge produs</button></td>
-      </tr>
+  <tbody class="tdata">
   </tbody>
 </table></div>`
+
+let tableInput = JSON.parse(localStorage.getItem('test2'))
+let tableHTML;
+
+tableInput.forEach(element => {
+  tableHTML+=` <tr>
+      <td>1</td> <td>${element.sistem}</td><td>${element.cod}</td><td>eticheta</td><td>Dimensiune</td><td>Buc</td><td>${element.cantitate}</td><td>${element.suprafata}</td><td>${element.suprafata}</td><td>${element.flansa}</td><td>${element.observatii}</td><td><button class="quantity-increase css-quantity-increase js-quantity-increase">+</button> <button class="js-quantity-decrease quantity-decrease css-quantity-decrease">-</button></td><td><button class="quantity-delete css-quantity-delete js-quantity-delete" data-productid=>Sterge produs</button></td>
+      </tr>`
+
+});
+
+document.querySelector('.tdata').innerHTML=tableHTML
