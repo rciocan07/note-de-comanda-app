a3aa3f089e08af355d1488c29d075b92a983c490
Robert Ciocan <ciocan.robertt@gmail.com>
2025-01-18 00:41:20 +0200

    Added all types of products

--- data/produse.js
+++ data/produse.js
@@ -11,7 +11,7 @@ export let dimParticulare =[
     },
     suprafata:(param)=>{
       console.log(param)
-      let result = param[0]*param[1]*param[2]
+      let result = (param[0]*param[1]*param[2])/100000000
       return result
     }
   },
@@ -27,7 +27,7 @@ export let dimParticulare =[
     },
     suprafata:(param)=>{
       console.log(param)
-      let result = param[0]+param[1]+param[2]+param[3]+param[4]
+      let result = (((2*(param[0]+param[3])*(param[1]+param[3])+param[2]*Math.PI*(param[3]+Math.max(param[0],param[1])/2))*param[4]/90)/1000000).toFixed(2)
       return result
     } 
     // (A,B,C)=>{
@@ -69,4 +69,214 @@ export let dimParticulare =[
       return result
     } 
   },
+  {
+    nume:"Cot Rectangular Drept",
+    cod:"RCP",
+    dimensiuni:{
+      dimensiuneA:"A1:",
+      dimensiuneB:"A2:",
+      dimensiuneC:"B:",
+      dimensiuneD:"R:",
+      dimensiunee:"<:",
+    },
+    suprafata:(param)=>{
+      console.log(param)
+      let result = param[0]+param[1]+param[2]+param[3]+param[4]
+      return result
+    } 
+  },
+  {
+    nume:"Cot Rectangular cu dirijori",
+    cod:"RC",
+    dimensiuni:{
+      dimensiuneA:"A1:",
+      dimensiuneB:"A2:",
+      dimensiuneC:"B:",
+      dimensiuneD:"R:",
+      dimensiunee:"<():",
+    },
+    suprafata:(param)=>{
+      console.log(param)
+      let result = param[0]+param[1]+param[2]+param[3]+param[4]
+      return result
+    } 
+  },
+  {
+    nume:"TEU",
+    cod:"RTE",
+    dimensiuni:{
+      dimensiuneA:"A:",
+      dimensiuneB:"B:",
+      dimensiuneC:"C:",
+      dimensiuneD:"R:",
+      dimensiunee:"L:",
+    },
+    suprafata:(param)=>{
+      console.log(param)
+      let result = param[0]+param[1]+param[2]+param[3]+param[4]
+      return result
+    } 
+  },
+  {
+    nume:"Piesa de deviatie (Etaj)",
+    cod:"RSD",
+    dimensiuni:{
+      dimensiuneA:"A:",
+      dimensiuneB:"B:",
+      dimensiuneC:"F:",
+      dimensiuneD:"L:",
+    },
+    suprafata:(param)=>{
+      console.log(param)
+      let result = param[0]+param[1]+param[2]+param[3]+param[4]
+      return result
+    } 
+  },
+  {
+    nume:"Piesa de deviatie (Etaj)",
+    cod:"RSD",
+    dimensiuni:{
+      dimensiuneA:"A:",
+      dimensiuneB:"B:",
+      dimensiuneC:"F:",
+      dimensiuneD:"L:",
+    },
+    suprafata:(param)=>{
+      console.log(param)
+      let result = param[0]+param[1]+param[2]+param[3]+param[4]
+      return result
+    } 
+  },
+  {
+    nume:"YAKA",
+    cod:"RPRR",
+    dimensiuni:{
+      dimensiuneA:"A:",
+      dimensiuneB:"B:",
+      dimensiuneC:"C:",
+      dimensiuneD:"L:",
+      dimensiuneD:"G:"
+    },
+    suprafata:(param)=>{
+      console.log(param)
+      let result = param[0]+param[1]+param[2]+param[3]+param[4]
+      return result
+    } 
+  },
+  {
+    nume:"Ram. Bilat. 2 Coturi",
+    cod:"RRB",
+    dimensiuni:{
+      dimensiuneA:"C1:",
+      dimensiuneB:"C2:",
+      dimensiuneC:"B:",
+      dimensiuneD:"R:",
+      dimensiuneD:"D1:",
+      dimensiuneF:"D2:",
+      dimensiuneG:"<():"
+    },
+    suprafata:(param)=>{
+      console.log(param)
+      let result = param[0]+param[1]+param[2]+param[3]+param[4]
+      return result
+    } 
+  },
+  {
+    nume:"Ram. Bilat. Cot + Canal",
+    cod:"RRL",
+    dimensiuni:{
+      dimensiuneA:"D1:",
+      dimensiuneB:"D2:",
+      dimensiuneC:"B:",
+      dimensiuneD:"R:",
+      dimensiuneD:"C1:",
+      dimensiuneF:"C2:",
+      dimensiuneG:"<():",
+      dimensiuneH:"L:"
+    },
+    suprafata:(param)=>{
+      console.log(param)
+      let result = param[0]+param[1]+param[2]+param[3]+param[4]
+      return result
+    } 
+  },
+  {
+    nume:"Ramificatie pantalon",
+    cod:"RRP",
+    dimensiuni:{
+      dimensiuneA:"A:",
+      dimensiuneB:"B:",
+      dimensiuneC:"C:",
+      dimensiuneD:"D:",
+      dimensiuneD:"E:",
+      dimensiuneF:"H:",
+      dimensiuneH:"L:"
+    },
+    suprafata:(param)=>{
+      console.log(param)
+      let result = param[0]+param[1]+param[2]+param[3]+param[4]
+      return result
+    } 
+  },
+  {
+    nume:"Capac",
+    cod:"RCA",
+    dimensiuni:{
+      dimensiuneA:"A:",
+      dimensiuneB:"B:"
+    },
+    suprafata:(param)=>{
+      console.log(param)
+      let result = param[0]+param[1]+param[2]+param[3]+param[4]
+      return result
+    } 
+  },
+  {
+    nume:"Schimbare Sectiune Concentrica",
+    cod:"RCSS",
+    dimensiuni:{
+      dimensiuneA:"fiD:",
+      dimensiuneB:"A:",
+      dimensiuneC:"B:",
+      dimensiuneD:"L:",
+      dimensiuneD:"alfa:"
+    },
+    suprafata:(param)=>{
+      console.log(param)
+      let result = param[0]+param[1]+param[2]+param[3]+param[4]
+      return result
+    } 
+  },
+  {
+    nume:"Schimbare Sectiune Excentrica",
+    cod:"SSE",
+    dimensiuni:{
+      dimensiuneA:"fiD:",
+      dimensiuneB:"A:",
+      dimensiuneC:"B:",
+      dimensiuneD:"L:",
+      dimensiuneD:"alfa:"
+    },
+    suprafata:(param)=>{
+      console.log(param)
+      let result = param[0]+param[1]+param[2]+param[3]+param[4]
+      return result
+    } 
+  },
+  {
+    nume:"Plenum",
+    cod:"PL",
+    dimensiuni:{
+      dimensiuneA:"A:",
+      dimensiuneB:"B:",
+      dimensiuneC:"H:",
+      dimensiuneD:"fi:",
+      dimensiuneD:"Nr. Stut:"
+    },
+    suprafata:(param)=>{
+      console.log(param)
+      let result = param[0]+param[1]+param[2]+param[3]+param[4]
+      return result
+    } 
+  }
 ]
