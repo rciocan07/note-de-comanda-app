e78d1f97689b5c8c17c25249fd9fb8baee2b6c40
Robert Ciocan <ciocan.robertt@gmail.com>
2025-01-20 17:53:10 +0200

    slight improvements to home page

--- scripts/dashboard.js
+++ scripts/dashboard.js
@@ -1,1 +1,4 @@
-document.querySelector('.produsbrutpage').addEventListener('click',()=>{window.open('./genprod-mobile.html', "_self")})
+document.querySelector('.produsbrutpage').addEventListener('click',()=>{window.open('./genprod.html', "_self")})
+document.querySelector('.produsbrutMobile').addEventListener('click',()=>{window.open('./genprod-mobile.html', "_self")})
+document.querySelector('.ventilatie').addEventListener('click',()=>{window.open('./ventilatie.html', "_self")})
+document.querySelector('.ventilatieMobile').addEventListener('click',()=>{window.open('./ventilatie-mobile.html', "_self")})
